3b878c400ef3254dd34b25047422bb3b7d3f747f
Mark Innes <innesm4@gmail.com>
2014-10-17 22:26:14 +0100

    tidy routes

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,9 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
+|-----------------------------------------------------------------------
+| Home
+|-----------------------------------------------------------------------
 */
 
 Route::get('/', array('as' => 'home', 'uses' => 'PageController@home'));
@@ -22,14 +17,7 @@ Route::get('/', array('as' => 'home', 'uses' => 'PageController@home'));
 
 Route::controller('users', 'UserController');
 
-/*
-|-----------------------------------------------------------------------
-| Groups
-|-----------------------------------------------------------------------
-*/
-
 Route::resource('groups', 'GroupController');
-
 
 
 /*
